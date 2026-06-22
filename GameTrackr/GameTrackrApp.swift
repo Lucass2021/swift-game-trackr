83b34bba5a9a3d60eb4210df080965181aa51e67
Lucas Dias <lucas.allx@hotmail.com>
2026-06-22 18:17:28 -0300

    Initial Commit

---
+++ GameTrackr/GameTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTrackrApp.swift
+//  GameTrackr
+//
+//  Created by Lucas Dias on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GameTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameTrackrTests/GameTrackrTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTrackrTests.swift
+//  GameTrackrTests
+//
+//  Created by Lucas Dias on 22/06/26.
+//
+
+import Testing
+@testable import GameTrackr
+
+struct GameTrackrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
